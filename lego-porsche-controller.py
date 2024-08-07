a7bb81ab5a7eececcf7011454bc2cda3daed48cc
toor <seboxxl@gmail.com>
2024-08-07 19:00:21 +0200

    PLAYVM used for controlling
    Idea from https://github.com/DanieleBenedettelli/TechnicMoveHub/

--- lego-porsche-controller.py
+++ lego-porsche-controller.py
@@ -7,27 +7,28 @@ from bleak import BleakClient, BleakScanner
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 
-# Define motors channels in bytes
+# Define channels in bytes
 drivemotor1 = 0x32
 drivemotor2 = 0x33
 steeringmotor = 0x34
-steeringangletolerance = 5
-steeringmaxangle = 0
+leds = 0x35
+playvm = 0x36
+hubled = 0x3F
+LIGHTS_OFF_OFF = 0b100
+LIGHTS_OFF_ON = 0b101
+LIGHTS_ON_ON = 0b000
+
+# Globals
+client = None
 debug = False
-last_power = 0
-calibrated = 0
+joystick = None
+last_power_input = 0
+last_steering_input = 0
+stop_event = asyncio.Event()
 
 DEVICE_NAME = "Technic Move  "
 CHARACTERISTIC_UUID = "00001624-1212-efde-1623-785feabcd123"
-#SERVICE_UUID = "00001624-1212-efde-1623-785feabcd123" #todo
-
-currentlysteering = 0
-target_angle = 0
-stop_event = asyncio.Event()
-last_steering_input = None
-
-def calculate_power_byte(power_input):
-	return int(power_input + 256 if power_input < 0 else power_input)
+#SERVICE_UUID = "00001624-1212-efde-1623-785feabcd123"
 
 def normalize_angle(angle):
 	if angle >= 180:
@@ -50,117 +51,14 @@ def bytes_to_angle(byte_array):
 	
 	# Handle negative values (32-bit signed integer)
 	if angle >= 0x80000000:
-		angle -= 0x100000000
-	
+		angle -= 0x100000000	
 	return angle
 
-async def set_drive_motor_power(channel, power_input):
-	power_byte = calculate_power_byte(power_input)
+async def set_drive_motor_power(channel, power_byte):
 	command = bytes([0x08, 0x00, 0x81, channel, 0x11, 0x51, 0x00, power_byte])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
+	await write_characteristic(command)
 	if debug:
 		print(f"Sent set_drive_motor_power command to channel {channel}: {command.hex()}")
-
-async def steering_goto_pos(inputangle=None, power=25):
-	global currentlysteering, target_angle, last_steering_input, calibrated, steeringmaxangle
-	while not stop_event.is_set() and calibrated == 1:
-		if inputangle == None:
-			angle = target_angle
-		else:
-			angle = inputangle
-			last_steering_input = 0
-			target_angle = angle
-		# Read data from the characteristic
-		if angle == last_steering_input and currentlysteering == 0:
-			await asyncio.sleep(0.05)
-			continue
-		data = await client.read_gatt_char(CHARACTERISTIC_UUID)
-		#print(f"Read data: {data.hex()}")		
-		if len(data) >= 8 and data[3] == steeringmotor:
-			current_angle_bytes = data[4:8]
-			current_angle = bytes_to_angle(current_angle_bytes)
-			if debug:
-				print(f"Current angle: {current_angle}, target angle: {angle}")
-			# Check if the current angle is satisfactory
-			if abs(current_angle - angle) <= steeringangletolerance:# or ((current_angle <= -steeringmaxangle+steeringangletolerance or current_angle >= steeringmaxangle-steeringangletolerance) and inputangle == None and (angle <= -steeringmaxangle+steeringangletolerance or angle >= steeringmaxangle-steeringangletolerance)):
-				command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, 0x00])
-				await write_characteristic(CHARACTERISTIC_UUID, command)
-				currentlysteering = 0
-				if debug:
-					print("Steering angle is close to target, stopping motor")
-				if inputangle != None:
-					break
-			else:
-				#if inputangle == None and abs(angle) >= steeringmaxangle-steeringangletolerance:#apply more power for steering close to maximum position
-				#	powermodified = 40
-				#	if debug:
-				#		print("Applying more power, requested angle: " + str(angle) + ", maxangle: " + str(steeringmaxangle-steeringangletolerance))
-				#else:
-				#	powermodified = abs(power)
-				powermodified = abs(power)
-				if current_angle < angle and currentlysteering <= 0:
-					command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, calculate_power_byte(powermodified)])
-					currentlysteering = 1
-					await write_characteristic(CHARACTERISTIC_UUID, command)
-				elif current_angle > angle and currentlysteering >= 0:
-					command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, calculate_power_byte(-powermodified)])
-					currentlysteering = -1
-					await write_characteristic(CHARACTERISTIC_UUID, command)		
-		last_steering_input = angle
-		await asyncio.sleep(0.05)
-		
-async def autocalibrate_steering():#0A004134020000000001
-	global steeringmaxangle, calibrated, target_angle, last_steering_input
-	calibrated = 0
-	print("===================================================")
-	print("Autocalibration started, PLEASE WAIT...")
-	print("It calibrates best when tires don't touch the")
-	print("ground, but it will need battery level correction")
-	print("anyway.")
-	await asyncio.sleep(3.0)
-	#subscribe to steering motor angle changes
-	command = bytes([0x0A, 0x00, 0x41, steeringmotor, 0x02, 0x00, 0x00, 0x00, 0x00, 0x01])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	#go right at 35% for 1.0s
-	command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, calculate_power_byte(20)])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(1.5)
-	command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, 0x00])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(0.3)
-	#reset encoder
-	command = bytes([0x0b, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x02, 0x00, 0x00, 0x00, 0x00])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(0.3)
-	#go left at 35% for 1.5s
-	command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, calculate_power_byte(-20)])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(1.5)
-	command = bytes([0x08, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x00, 0x00])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(0.3)
-	#calculate total move degrees - get position, which which is equal to -total movement range
-	data = await read_characteristic(CHARACTERISTIC_UUID)
-	steeringrange = -(bytes_to_angle(data[4:8]))
-	if debug:
-		print("Steering range: " + str(steeringrange) + " deg.")
-	#goto centered angle
-	tempcenteredangle = -(int(steeringrange/2))
-	calibrated = 1
-	await steering_goto_pos(tempcenteredangle, 20)
-	await asyncio.sleep(0.5)	
-	#reset encoder
-	command = bytes([0x0b, 0x00, 0x81, steeringmotor, 0x11, 0x51, 0x02, 0x00, 0x00, 0x00, 0x00])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
-	await asyncio.sleep(0.3)
-	steeringmaxangle = int(steeringrange/2)
-	#print("steeringmaxangle set to: ", steeringmaxangle)
-	target_angle = 0
-	last_steering_input = 0
-	await asyncio.sleep(0.6)
-	print("Autocalibration FINISHED. You can drive now!")
-	print("===================================================")
-	#0b00813411510200000000 reset encoder
 
 async def connect_to_device(name):
 	global client
@@ -172,12 +70,17 @@ async def connect_to_device(name):
 			return client
 	return None
 
-async def read_characteristic(characteristic_uuid):
-	data = await client.read_gatt_char(characteristic_uuid)
+async def read_characteristic():
+	data = await client.read_gatt_char(CHARACTERISTIC_UUID)
 	return data
 
-async def write_characteristic(characteristic_uuid, data):
-	await client.write_gatt_char(characteristic_uuid, data)
+async def write_characteristic(data):
+	try:
+		await client.write_gatt_char(CHARACTERISTIC_UUID, data)	
+	except Exception as e:
+		print(f"Failed to write data  {data.hex()}: {e}")
+	else:
+		print(f"{data.hex()} written")
 
 def process_hub_property_data(data):
 	if data is None or len(data) < 6:
@@ -249,125 +152,162 @@ def process_read_data(data):
 		elif message_type == 0x45:
 			port = data[3]
 			buffer = buffer + "[Port Input subscribed data] Port " + str(int(port))
-			if port == steeringmotor and len(data) == 8:#for our script, we assume we're getting steering angle here
+			if port == steeringmotor and len(data) == 8:# For our script, we assume we're getting steering angle here
 				angle = data[4:8]
 				angle = bytes_to_angle(angle)
 				buffer = buffer + " angle: " + str(angle) + "deg"
+		elif message_type == 0x47:
+			port = data[3]
+			buffer = buffer + "[Port Input subscribtion changed] Port " + str(int(port))
 	print(f"Read data: {data.hex()} {buffer}")
 
-async def request_hub_properties(characteristic_uuid):
-	await asyncio.sleep(0.3)
-	await write_characteristic(characteristic_uuid, bytes.fromhex("0500010305"))
-	fw_data = await read_characteristic(characteristic_uuid)
+async def initialize_hub():
+	await write_characteristic(bytes.fromhex("0500010305"))
+	fw_data = await read_characteristic()
 	firmware_version = process_hub_property_data(fw_data)
 
-	await asyncio.sleep(0.3)
-	await write_characteristic(characteristic_uuid, bytes.fromhex("0500010405"))
-	hw_data = await read_characteristic(characteristic_uuid)
+	await write_characteristic(bytes.fromhex("0500010405"))
+	hw_data = await read_characteristic()
 	hardware_version = process_hub_property_data(hw_data)
 
-	await asyncio.sleep(0.3)
-	await write_characteristic(characteristic_uuid, bytes.fromhex("0500010605"))
-	battery_data = await read_characteristic(characteristic_uuid)
+	await write_characteristic(bytes.fromhex("0500010605"))
+	battery_data = await read_characteristic()
 	battery_percentage = process_hub_property_data(battery_data)
 	
-	await asyncio.sleep(0.3)
-	await write_characteristic(characteristic_uuid, bytes.fromhex("0800813F11510006"))#change hub led color to green, so we're sure we're in control
+	# Change hub led color to green, so we're sure we're in control
+	await asyncio.sleep(1.0)
+	await write_characteristic(bytes.fromhex("0800813F11510006"))
 	
-	await asyncio.sleep(3.0)
-	await write_characteristic(characteristic_uuid, bytes.fromhex("09008135115100ff00"))#turn off 6led so we won't waste battery for now (led mask, power int 0-100)
+	print(f"Connected to {DEVICE_NAME}")
+	print(f"Firmware Version: {firmware_version}")
+	print(f"Hardware Version: {hardware_version}")
+	print(f"Battery level: {battery_percentage}%\n")	
+	await autocalibrate_steering()
 
-	return firmware_version, hardware_version, battery_percentage
+	# Turn off 6led so we won't waste battery for now (led mask, power int 0-100) - this needs to be delayed, as the leds blink after connection first few seconds
+	await write_characteristic(bytes.fromhex("09008135115100ff00"))
+
+async def initialize_joystick():
+	global joystick
+	pygame.joystick.init()
+	
+	if pygame.joystick.get_count() == 0:
+		print("No joystick found.")#todo joystick reinitialization and selection in separate function with available command
+	else:
+		joystick = pygame.joystick.Joystick(0)
+		joystick.init()	
+		print(f"Joystick name: {joystick.get_name()}")
+
+async def autocalibrate_steering():
+	print("Calibrating steering...")
+	await write_characteristic(bytes.fromhex("0d008136115100030000001000"))
+	await write_characteristic(bytes.fromhex("0d008136115100030000000800"))
+	await asyncio.sleep(0.5)
+	print("Car is READY!")
 
 async def handle_controller_events():
-	global power_input, last_power, target_angle, last_steering_input
-	powerchanged = False
-	steeringchanged = False
+	global last_power_input, last_steering_input
 	for event in pygame.event.get():
 		if event.type == pygame.JOYAXISMOTION:
-			left_trigger = joystick.get_axis(4)
-			right_trigger = joystick.get_axis(5)
-			power_input = int(((-left_trigger + right_trigger) * 100) / 2)
-			if abs(power_input) < 20:
-				power_input = 0
-			if abs(power_input - last_power) >= 5:
-				powerchanged = True
-				last_power = power_input
-				await asyncio.create_task(set_drive_motor_power(drivemotor1, -power_input))
-				await asyncio.create_task(set_drive_motor_power(drivemotor2, power_input))
-			input_angle = int(joystick.get_axis(0)*steeringmaxangle)
-			if abs(input_angle - last_steering_input) >= steeringangletolerance:
-				steeringchanged = True
-				target_angle = input_angle
-			if ((powerchanged or steeringchanged) and debug):
-				print(f"power_input: {power_input}, steering_input: {target_angle}")
+			
+			power_input = int(((joystick.get_axis(5)-joystick.get_axis(4)) * 100) / 2)
+			power_input_modified = power_input
+			if abs(power_input_modified) <= 10:
+				power_input_modified = 0
+			
+			steering_input = int(joystick.get_axis(0)*100)
+			steering_input_modified = steering_input
+			if abs(steering_input_modified) <= 1:
+				steering_input_modified = 0
+
+			if debug:
+				print(f"power_input: {power_input} (modified: {power_input_modified}), steering_input: {steering_input} (modified: {steering_input_modified})")
+
+			# Ignore power_input change for less than 5% and steering less than 3%
+			power_input_changed = False
+			steering_input_changed = False
+			if abs(power_input_modified - last_power_input) >= 5:
+				power_input_changed = True
+				last_power_input = power_input_modified
+			if abs(steering_input_modified - last_steering_input):
+				steering_input_changed = True
+				last_steering_input = steering_input_modified
+
+			# Send PLAYVM commands
+			if (power_input_changed or steering_input_changed) and client != None and client.is_connected():
+				lights = LIGHTS_OFF_OFF
+				command = bytes([0x0d, 0x00, 0x81, 0x36, 0x11, 0x51, 0x00, 0x03, 0x00, power_input_modified&0xFF, steering_input_modified&0xFF, lights&0xFF, 0x00])
+				await write_characteristic(command)
+			elif debug:
+				print("Drive commands ignored due to low input change or HUB disconnected!")
+				
+				
 async def read_input():
 	loop = asyncio.get_event_loop()
 	while True:
-		command = await loop.run_in_executor(None, input, "Enter data to write or type 'read' / 'exit' or use controller:\n")
+		command = await loop.run_in_executor(None, input, "Enter data to write, use controller or type 'help':\n")
 		yield command.strip().lower()
 
+def debug_mode(status=None):
+	global debug
+	if status != None:
+		debug = status
+	print("Debug mode is:", "ON" if debug else "OFF")
+
 async def handle_terminal_commands(stop_event):
-	current_characteristic_uuid = CHARACTERISTIC_UUID
 	async for cmd in read_input():
 		if cmd == "exit":
 			stop_event.set()
 			break
 		elif cmd == "read":
-			data = await read_characteristic(current_characteristic_uuid)
+			data = await read_characteristic()
 			process_read_data(data)
+		elif cmd == "debug":
+			debug_mode()
+		elif cmd == "debugon":
+			debug_mode(True)
+		elif cmd == "debugoff":
+			debug_mode(False)
+		elif cmd == "autocalibrate":
+			await autocalibrate_steering()
+		elif cmd == "joystick":
+			await initialize_joystick()
+		elif cmd == "help":
+			print("Available commands: exit, read, debug, debugon, debugoff, autocalibrate, joystick, help")#todo: voltage, battery, temp
 		else:
 			try:
 				data_bytes = bytes.fromhex(cmd)
-				await write_characteristic(current_characteristic_uuid, data_bytes)
+				await write_characteristic(data_bytes)
 				print("Data written")
 				if debug:
-					data = await read_characteristic(current_characteristic_uuid)
+					data = await read_characteristic()
 					process_read_data(data)
 			except ValueError:
-				print("Invalid hex data format")
+				print("Invalid hex data format or unknown command")
 
 async def main():
-	global joystick
+	global client, joystick
 	print("Searching for LEGO Porsche car. Make sure it's on and blinking...")
-
+	debug_mode()
 	client = await connect_to_device(DEVICE_NAME)
 	if client is None:
 		print(f"Device '{DEVICE_NAME}' not found.")
 		return
 
-	await client.pair()
-	firmware_version, hardware_version, battery_percentage = await request_hub_properties(CHARACTERISTIC_UUID)
-	print(f"Connected to {DEVICE_NAME}")
-	print(f"Firmware Version: {firmware_version}")
-	print(f"Hardware Version: {hardware_version}")
-	print(f"Battery level: {battery_percentage}%")
+	await client.pair(protection_level = 2)
+	await initialize_hub()
 
 	pygame.init()
-	pygame.joystick.init()
+	await initialize_joystick()
 	
-	if pygame.joystick.get_count() == 0:
-		print("No joystick found")
-
-	else:
-		joystick = pygame.joystick.Joystick(0)
-		joystick.init()	
-		print(f"Joystick name: {joystick.get_name()}")
-	
-	await autocalibrate_steering()
-
 	try:
 		await asyncio.gather(
 			handle_terminal_commands(stop_event),
-			controller_event_loop(stop_event),
-			asyncio.create_task(steering_goto_pos())
+			controller_event_loop(stop_event)
 		)
 	except KeyboardInterrupt:
 		pass
 	
-	#unsubscribe to steering motor angle changes
-	command = bytes([0x0A, 0x00, 0x41, steeringmotor, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00])
-	await write_characteristic(CHARACTERISTIC_UUID, command)
 	await client.disconnect()
 	print("Disconnected")
 
@@ -385,3 +325,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	debug = args.debug
 	asyncio.run(main())
+
+#todo telemetry
+#todo remember 6leds state for drive with playvm command
+#todo brake
