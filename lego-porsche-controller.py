3908439b752e7bea1087a0771c9265b7e0aa664a
toor <seboxxl@gmail.com>
2024-08-14 21:27:18 +0200

    Autocalibration fix for latest disabling 6LED with PLAYVM

--- lego-porsche-controller.py
+++ lego-porsche-controller.py
@@ -18,7 +18,7 @@ last_steering_input = 0
 stop_event = asyncio.Event()
 powerlimit = 100
 brakeapplied = False
-lightsplayvmstate = LIGHTS_OFF_OFF
+lightsplayvmstate = PLAYVM_LIGHTS_OFF_OFF
 last_lights_input = False
 cabinlights = False
 last_cabin_lights_input = False
@@ -257,13 +257,13 @@ async def initialize_hub():
 	print(f"Hardware Version: {hardware_version}")
 	print(f"LWP Version: {lwp_version}")
 	await get_battery_info()
-	await get_temperature()
+	await get_temperature()	
 	await autocalibrate_steering()
 	power_limit()
 
 	# Turn off 6led so we won't waste battery for now (led mask, power int 0-100)
 	#await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_6LEDS, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0xff, 0x00))
-	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, LIGHTS_OFF_OFF, 0x00))
+	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, PLAYVM_LIGHTS_OFF_OFF, 0x00))
 
 async def initialize_joystick():
 	global joystick
@@ -279,8 +279,9 @@ async def initialize_joystick():
 async def autocalibrate_steering():
 	print("Calibrating steering...")
 	# "Special" PLAYVM commands to calibrate steering
-	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, 0x10, 0x00))
-	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00))
+	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, PLAYVM_COMMAND, 0x00))
+	await write_characteristic(create_command(PORT_OUTPUT_COMMAND, PORT_PLAYVM, FEEDBACK_ACTION_BOTH, PORT_OUTPUT_SUBCOMMAND_WRITE_DIRECT, PORT_MODE_0, 0x03, 0x00, 0x00, 0x00, PLAYVM_CALIBRATE_STEERING, 0x00))
+	await asyncio.sleep(1.5) # we need to wait unti calibration is finished before sending any more PLAYVM commands
 	print("Car is READY!")
 
 async def handle_controller_events():
@@ -350,13 +351,13 @@ async def handle_controller_events():
 						print("lights_input and not last_lights_input")
 					lights_state_changed = True
 					last_lights_input = True
-					if lightsplayvmstate == LIGHTS_ON_ON or lightsplayvmstate == LIGHTS_ON_BRAKING:
-						lightsplayvmstate = LIGHTS_OFF_OFF
+					if lightsplayvmstate == PLAYVM_LIGHTS_ON_ON or lightsplayvmstate == PLAYVM_LIGHTS_ON_BRAKING:
+						lightsplayvmstate = PLAYVM_LIGHTS_OFF_OFF
 						if debug:
 							print("Lights changed from LIGHTS_ON_ON to LIGHTS_OFF_OFF")						
-					elif lightsplayvmstate == LIGHTS_OFF_OFF or lightsplayvmstate == LIGHTS_OFF_BRAKING:
+					elif lightsplayvmstate == PLAYVM_LIGHTS_OFF_OFF or lightsplayvmstate == PLAYVM_LIGHTS_OFF_BRAKING:
 						lights_state_changed = True
-						lightsplayvmstate = LIGHTS_ON_ON
+						lightsplayvmstate = PLAYVM_LIGHTS_ON_ON
 						if debug:
 							print("Lights changed from LIGHTS_OFF_OFF to LIGHTS_ON_ON")
 			if event.type == pygame.JOYBUTTONUP and last_lights_input:
@@ -368,7 +369,7 @@ async def handle_controller_events():
 			# Ignore power_input change for less than 5% and steering less than 3%
 			power_input_changed = False
 			steering_input_changed = False
-			if abs(power_input_modified - last_power_input) >= 5:
+			if abs(power_input_modified - last_power_input) >= 5:# todo this is fine fo 100% power, but not for 25%, so changed it to int(5*powerlimit*0.01)
 				power_input_changed = True
 				last_power_input = power_input_modified
 			if abs(steering_input_modified - last_steering_input) >= 3:
@@ -376,10 +377,10 @@ async def handle_controller_events():
 				last_steering_input = steering_input_modified
 
 			if brake_input:
-				if lightsplayvmstate == LIGHTS_OFF_OFF:
-					lights = LIGHTS_OFF_BRAKING
-				elif lightsplayvmstate == LIGHTS_ON_ON:
-					lights = LIGHTS_ON_BRAKING
+				if lightsplayvmstate == PLAYVM_LIGHTS_OFF_OFF:
+					lights = PLAYVM_LIGHTS_OFF_BRAKING
+				elif lightsplayvmstate == PLAYVM_LIGHTS_ON_ON:
+					lights = PLAYVM_LIGHTS_ON_BRAKING
 			else: 
 				lights = lightsplayvmstate
 			# Send PLAYVM commands
@@ -546,3 +547,5 @@ if __name__ == "__main__":
 
 #todo telemetry
 #todo fix stalling drive motor on low power and fast steering inputs + drive direction changes
+#todo better inputs, considering power limit
+#todo rumble effects
